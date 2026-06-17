882d722bb443e9bf5b73d73e73c7ee9f78581f5f
ya <abesate234@gmail.com>
2026-06-17 13:57:23 +0900

    vue-chart

---
+++ resources/js/Pages/Analysis.vue
@@ -0,0 +1,42 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { reactive, onMounted} from 'vue'
+import { getToday } from '@/common'
+
+onMounted(() => {
+    form.startDate = getToday()
+    form.endDate = getToday()
+})
+
+const form = reactive({
+    startDate: null,
+    endDate: null
+})
+</script>
+
+<template>
+    <Head title="データ分析" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">データ分析</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <form>
+                            From: <input type="date" name="startDate" v-model="form.startDate">
+                            To: <input type="date" name="endDate" v-model="form.startDate"><br>
+                            <button class="rounded-md bg-indigo-600 text-sm font-semibold text-white shadow-sm">
+                                分析する
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
